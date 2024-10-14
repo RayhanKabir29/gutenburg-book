8b7254587c5ca4919011796f8bbf6a9603a8259a
Rayhan Kabir <rayhan.kabir29@gmail.com>
2024-10-15 01:23:47 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const url = 'https://gutendex.com/books';
+async function fetchBooks() {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        const books = data?.results;
+        console.log(data);
+        displayBooks(books);
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+
+const displayBooks = books =>{
+     console.log(books);
+    const bookContainer = document.getElementById('book-grid')
+    books.forEach(book => {
+         console.log(book.id);
+        const bookCard = document.createElement('div');
+        bookCard.className=`book-card`;
+        bookCard.innerHTML=`<img src=${book?.formats?.
+            image/jpeg} alt="Book Image">
+                <h3>${book.title}</h3>
+                <p>Author: Author Name</p>
+                <p>Summary of the book goes here.</p>`;
+                bookContainer.appendChild(bookCard);
+    });
+}
+fetchBooks();
+
